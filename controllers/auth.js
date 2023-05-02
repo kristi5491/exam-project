3aa7b2afa6cdc9712a94c9366abd52add3c20197
Chuliyka <116159308+Chuliyka@users.noreply.github.com>
2023-05-02 18:14:56 +0300

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,42 +4,41 @@ import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 
-export const register = async (req,res,next) => {
-    try {
-            // Store hashh in your password DB !
-            const salt = bcrypt.genSaltSync(10);
-         const hash = bcrypt.hashSync (req.body.password,salt);
+export const register = async (req, res, next) => {
+  try {
+    // Store hashh in your password DB !
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-       const newUser = new User ({
-     username: req.body.username,
-     email: req.body.email,
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
       password: hash,
-        }); 
+    });
 
-        await newUser.save ();
-        res.status(200).send("User has been created.");
-    } catch(err) { 
-        next (err);
-    }
-}; 
+    await newUser.save();
+    res.status(200).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
+};
 
 // login
 export const login = async (req, res, next) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-      if (!user)  return next(createError(404, "User not found!"));
-  
-      const isPasswordCorrect = await bcrypt.compare(req.body.password,user.password);
-      if (!isPasswordCorrect) 
-        return next(createError(400, "Wrong password or username!"));
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found!"));
 
-        const token = jwt.sign( { id: user._id, isAdmin: user.isAdmin }, "sdfsdfs" );
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
-        const { password, isAdmin, ...otherDetails } = user._doc;
-        res.status(200).json ({...otherDetails});
-    } catch (err) {
-        next(err);
-      }
-    };
-   
-   
+    const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin });
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json({ ...otherDetails });
+  } catch (err) {
+    next(err);
+  }
+};
+
