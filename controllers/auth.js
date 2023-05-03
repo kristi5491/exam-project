dc37cd686ac723f69e88f9ef120058cc25bd4c55
kristi5491 <ksavchin05@gmail.com>
2023-05-03 15:11:28 +0300

    save3

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,10 +33,14 @@ export const login = async (req, res, next) => {
       if (!isPasswordCorrect) 
         return next(createError(400, "Wrong password or username!"));
 
-        const token = jwt.sign( { id: user._id, isAdmin: user.isAdmin }, "sdfsdfs" );
+        const token = jwt.sign( { id: user._id, isAdmin: user.isAdmin }, process.env.JWT);
 
         const { password, isAdmin, ...otherDetails } = user._doc;
-        res.status(200).json ({...otherDetails});
+        res.cookie("access_token", token,{
+          httpOnly:true,
+        })
+        .status(200)
+        .json ({...otherDetails});
     } catch (err) {
         next(err);
       }
